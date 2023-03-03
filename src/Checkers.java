c59c828629d492ad7cadabde4ce89a588dddce41
Ben <hackerb@lawrence.edu>
2023-03-03 09:41:52 -0600

    Added checkers class

---
+++ src/Checkers.java
@@ -0,0 +1,6 @@
+public class Checkers { // Plays a game of checkers using Piece and Board classes
+    public static void main(String[] args) { // args could take in # of human players, who plays which color, and board size and color
+        Piece piece = new Piece();
+        System.out.println(piece); // Temporary test of toString method
+    }
+}
