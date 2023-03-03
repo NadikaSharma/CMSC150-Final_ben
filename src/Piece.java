7203d3e15f6e2f925cf3b066cc2bc472c3c3e090
Ben <hackerb@lawrence.edu>
2023-03-03 09:39:42 -0600

    Added piece class

---
+++ src/Piece.java
@@ -0,0 +1,39 @@
+public class Piece { // Represents a game piece used for checkers or chess
+    private char state; // Capital letter if black, lowercase if white, m if man, k if king
+    private String coords; // Coordinates of piece on an 8x8 board where A-H is left to right and 1-8 is bottom to top
+
+    public Piece() { // Creates a black man piece in the bottom left corner
+        this.state = 'M';
+        this.coords = "A1";
+    }
+
+    public Piece(char state) { // Creates a piece with given a state
+        this.state = state;
+
+        if (Character.isUpperCase(state)) { // Places piece in bottom left corner if black, top right if white
+            this.coords = "A1";
+        } else {
+            this.coords = "H8";
+        }
+    }
+
+    public Piece(char state, String coords) { // Creates a piece with given a state and coords
+        this.state = state;
+        this.coords = coords;
+    }
+
+    public String getCoords() {
+        return this.coords;
+    }
+
+    public void setCoords(String coords) {
+        this.coords = coords;
+    }
+
+    public String toString() { // Returns state as a string if it is a valid piece, returns "?" otherwise
+        if (Character.toLowerCase(this.state) == 'm' || Character.toLowerCase(this.state) == 'k') {
+            return "" + this.state;
+        }
+        return "?";
+    }
+}
